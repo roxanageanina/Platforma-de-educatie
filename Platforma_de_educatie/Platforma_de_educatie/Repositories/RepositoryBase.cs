ed7399f51c858d5a14d888eef19213f776823c21
roxanageanina <andreiroxana96@yahoo.com>
2025-01-07 20:52:22 +0200

    Add files via upload

---
+++ Platforma_de_educatie/Platforma_de_educatie/Repositories/RepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Platforma_de_educatie.Data;
+
+namespace Platforma_de_educatie.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        protected DataClassesDataContext GetContext()
+        {
+            var context = new DataClassesDataContext();
+            Console.WriteLine("Conexiunea cu baza de date s-a realizat cu succes!");
+            return context;
+        }
+    }
+}
---
+++ Platforma_de_educatie/Platforma_de_educatie/ViewModels/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Platforma_de_educatie.Models;
+using Platforma_de_educatie.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Platforma_de_educatie.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        //Fields
+        private UserAccountModel _currentUserAccount;
+        private IUserRepository userRepository;
+
+        public UserAccountModel CurrentUserAccount
+        {
+            get
+            {
+                return _currentUserAccount;
+            }
+
+            set
+            {
+                _currentUserAccount = value;
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            }
+        }
+
+        public MainViewModel()
+        {
+            userRepository = new UserRepository();
+            CurrentUserAccount = new UserAccountModel();
+            LoadCurrentUserData();
+        }
+
+        private void LoadCurrentUserData()
+        {
+            var user = userRepository.GetByUsername(Thread.CurrentPrincipal.Identity.Name);
+            if (user != null)
+            {
+                CurrentUserAccount.Username = user.Username;
+                CurrentUserAccount.DisplayName = $"Bine ai venit, {user.Name} {user.LastName}!";
+                CurrentUserAccount.ProfilePicture = null; 
+            }
+            else
+            {
+                CurrentUserAccount.DisplayName = "Utilizator invalid, nu este logat.";
+            }
+        }
+    }
+}
